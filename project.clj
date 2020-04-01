0a1740b0798845ea457bea50f28e1468588529c6
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-04-01 12:49:16 +0100

    release 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject my-project/lein-template "0.2.2"
+(defproject my-project/lein-template "0.2.4"
   :description "Leiningen template for personal project."
   :dependencies [[clj-http "3.9.1"]
                  [cheshire "5.8.1"]]
